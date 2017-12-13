ef0578bea422bb44d2c7e6127b1a6d4d58c3cb5e
KvisazGames <kiko@kiko.kiko>
2017-12-13 20:48:49 +0700

    no float positions

--- js/PhaserConfig.js
+++ js/PhaserConfig.js
@@ -10,6 +10,8 @@ var PhaserConfig = {
         /*game.scale.pageAlignHorizontally = true; // не нужно при выравнивании через CSS
         game.scale.pageAlignVertically = true;*/
         game.input.maxPointers = 1; // мультитач не интересует
+        game.renderer.renderSession.roundPixels = true; // нахер пошли сотые доли пикселов
+        // Phaser.Canvas.setImageRenderingCrisp(game.canvas); // только для пиксельной графики в точном или пропорциональном 2м масштабе
         game.tweens.frameBased = false; // анимации на основе миллисекунд, а не фреймов, по умолчанию и так стоит false
         game.time.advancedTiming = true; // true - позволяет мерять  fps rate, fps min/max, suggestedFps and msMin/msMax. Недорого
         game.forceSingleUpdate = true; // true - Should the game loop force a logic update, regardless of the delta timer
