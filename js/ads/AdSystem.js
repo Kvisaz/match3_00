7b96f6412d6075e915f91b203b197e3d4786e01f
KvisazGames <kiko@kiko.kiko>
2017-12-16 04:28:16 +0700

    show banner 3

--- js/ads/AdSystem.js
+++ js/ads/AdSystem.js
@@ -1,28 +1,25 @@
 /**
  * Created by Work on 16.12.2017.
  */
-var Appodeal = Appodeal || undefined;
 var AdSystem = {};
 
 AdSystem.init = function () {
-    alert("Appodeal init");
-    if (Appodeal === undefined) return;
     var appKey = "6dbdcd8af4f87460fee0c08f67aafd7cb9ba53264646147c";
+    Appodeal.setTesting(true); // set test mode enabled || disabled
     Appodeal.disableLocationPermissionCheck();
     Appodeal.initialize(appKey, Appodeal.BANNER);
     Appodeal.enableBannerCallbacks(true);
-    document.addEventListener('onBannerLoaded', function(){
-        AdSystem.showBanner();
-    });
-
     alert("Appodeal initialized");
 };
 
 AdSystem.showBanner = function () {
-    if (Appodeal === undefined) return;
-
-    alert("Appodeal showBanner");
-    Appodeal.isLoaded(Appodeal.BANNER, function (result) {
-        Appodeal.show(Appodeal.BANNER_BOTTOM);
+    Appodeal.canShow(Appodeal.BANNER_BOTTOM, function(result) { // check if BANNER_BOTTOM can be shown for 'default' placement
+        if(result) { // returns true or false
+            alert("Appodeal can showBanner");
+            Appodeal.show(Appodeal.BANNER_BOTTOM);
+        }
+        else {
+            alert("Appodeal cannot showBanner");
+        }
     });
 };
