747547985136893650f2e646d6827bfec1686166
KvisazGames <kiko@kiko.kiko>
2017-12-16 04:43:21 +0700

    try catch for AdSystem

--- js/ads/AdSystem.js
+++ js/ads/AdSystem.js
@@ -5,22 +5,33 @@ var AdSystem = {};
 
 AdSystem.init = function () {
     var appKey = "6dbdcd8af4f87460fee0c08f67aafd7cb9ba53264646147c";
-    Appodeal.setTesting(true); // set test mode enabled || disabled
-    Appodeal.disableLocationPermissionCheck();
-    Appodeal.initialize(appKey, Appodeal.BANNER);
-    Appodeal.enableBannerCallbacks(true);
-    alert("Appodeal initialized");
+    try{
+        Appodeal.setTesting(true); // set test mode enabled || disabled
+        Appodeal.disableLocationPermissionCheck();
+        Appodeal.initialize(appKey, Appodeal.BANNER);
+        Appodeal.enableBannerCallbacks(true);
+    }
+    catch (e){
+
+    }
+
+   // alert("Appodeal initialized");
 };
 
 AdSystem.showBanner = function () {
     alert("Appodeal showBanner start");
-    Appodeal.isLoaded(Appodeal.BANNER_BOTTOM, function(result) { // check if BANNER_BOTTOM can be shown for 'default' placement
-        if(result) { // returns true or false
-            alert("Appodeal can showBanner");
-            Appodeal.show(Appodeal.BANNER_BOTTOM);
-        }
-        else {
-            alert("Appodeal cannot showBanner");
-        }
-    });
+    try {
+        Appodeal.isLoaded(Appodeal.BANNER_BOTTOM, function (result) { // check if BANNER_BOTTOM can be shown for 'default' placement
+            if (result) { // returns true or false
+                // alert("Appodeal can showBanner");
+                Appodeal.show(Appodeal.BANNER_BOTTOM);
+            }
+            else {
+                // alert("Appodeal cannot showBanner");
+            }
+        });
+    }
+    catch (e){
+
+    }
 };
